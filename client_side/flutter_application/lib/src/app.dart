8f4209ebf5d24468b5b79d868bc3a29f55fcda4d
Yuki Mishima <41721281+s1534@users.noreply.github.com>
2022-09-20 16:31:43 +0900

    a

--- client_side/flutter_application/lib/src/app.dart
+++ client_side/flutter_application/lib/src/app.dart
@@ -10,10 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      // title: 'Flutter Demo',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      darkTheme: ThemeData.dark(),
       home: const MyStatefulWidget(),
     );
   }
--- client_side/flutter_application/lib/src/screens/home.dart
+++ client_side/flutter_application/lib/src/screens/home.dart
@@ -1,7 +1,35 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'dart:convert';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'tmp.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+  @override
+  _HomeScreen createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  String data = "aaaaa";
+  Future<dynamic> getData() async {
+    http.Response response;
+
+    response = await http.get(Uri.parse('http://127.0.0.1:5000/model'));
+    // print(response.statusCode);
+    // setState(() {
+    //     // data = jsonDecode(response.body);
+    //     data = response.statusCode.toString();
+    //   });
+
+    if (response.statusCode == 200) {
+      setState(() {
+        // data = jsonDecode(response.body);
+        data = "aaaaaaaaaaaaaaaaaaaaaaa";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +38,32 @@ class HomeScreen extends StatelessWidget {
         title: const Text('temp'),
         backgroundColor: Color(0xff02d39a),
       ),
-      body:
-          const Center(child: Text('temp画面', style: TextStyle(fontSize: 32.0))),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(data),
+            ElevatedButton(
+              child: const Text('Button'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                onPrimary: Colors.white,
+              ),
+              onPressed: () {
+                getData();
+                // setState(() {
+                //   // data = jsonDecode(response.body);
+                //   data = "aaaaaaaaaaaaaaaaaaaaaaa";
+                // });
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => NextPage()),
+                // );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  
 }
